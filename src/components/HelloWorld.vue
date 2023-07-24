74f3587f1cf7cb5f45dbb94de094d85558eb9c03
bhagat-infiniteit <bhagat@infiniteit.biz>
2023-07-24 14:40:37 +0530

    Vue 3 setup and homepage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,73 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 :class="[isAddClass ? 'hello1' : '', 'hii']">{{ msg }}</h1>
+    <span v-once style="color: seagreen;"> {{ msg }}</span>
+    <span v-pre> {{ msg }}</span>
+  <br>
+    Name:-{{fname}}
+
+    <input type="text" v-model.lazy="fname">
+    <button v-on:click.once="handleClick">Click me </button>
+
+    <a v-on:click.stop="doThis">stop click</a>
+
+
+    <span v-if="show">
+      <p>
+        For a guide and recipes on how to configure / customize this project,<br>
+        check out the
+        <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      </p>
+      <h3>Installed CLI Plugins</h3>
+      <ul>
+        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      </ul>
+      <h3>Essential Links</h3>
+      <ul>
+        <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+        <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+        <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+        <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+        <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      </ul>
+      <h3>Ecosystem</h3>
+      <ul>
+        <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+        <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+        <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+        <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      </ul>
+    </span>
+    <ChilComponentVue></ChilComponentVue>
   </div>
+  
 </template>
 
-<script>
+<script >
+import ChilComponentVue from './ChildComponent.vue';
+
 export default {
   name: 'HelloWorld',
+  components: {ChilComponentVue},
   props: {
     msg: String
+  },
+  data(){
+    return {
+      show : false,
+      fname : null,
+      isAddClass : true
+    }
+  },
+  methods :{
+    handleClick(){
+      console.log('handleClick');
+    },
+    doThis(){
+      alert();
+    }
   }
 }
 </script>
